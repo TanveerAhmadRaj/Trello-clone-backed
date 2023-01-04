b46b7e5dfe9bdd0909c2052e7b90487435e166d0
TanveerAhmadRaj <ta78296@gmail.com>
2023-01-04 13:21:43 +0500

    Resend

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 // =============== initiating the instance of express ==========
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 //================ middlewares ================================
 
 app.use(express.json());
